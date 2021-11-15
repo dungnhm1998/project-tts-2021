8bb77828ce0c9fc577165383caf6b74baf4c0cc2
NudePeace <lastglint@gmail.com>
2021-11-15 09:11:16 +0700

    Update LoginHandler.java

--- src/main/java/com/app/tts/server/handler/base/LoginHandler.java
+++ src/main/java/com/app/tts/server/handler/base/LoginHandler.java
@@ -1,5 +1,18 @@
 package com.app.tts.server.handler.base;
 
-public class LoginHandler {
+import com.app.tts.session.redis.SessionStore;
 
+import io.vertx.core.Handler;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+public class LoginHandler implements Handler<RoutingContext>, SessionStore{
+	@Override
+	public void handle(RoutingContext routingContext) {
+
+		routingContext.vertx().executeBlocking(future -> {
+			try {
+				
+			}
+		});
+	}
 }
