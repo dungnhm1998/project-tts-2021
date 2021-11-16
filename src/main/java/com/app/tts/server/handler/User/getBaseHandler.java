123fc7386bc02c6c3dc6a72e8a30a26b0a7e53f2
nguyễn quý <94169574+chubekho@users.noreply.github.com>
2021-11-16 14:10:23 +0700

    get base

--- src/main/java/com/app/tts/server/handler/User/getBaseHandler.java
+++ src/main/java/com/app/tts/server/handler/User/getBaseHandler.java
@@ -3,10 +3,10 @@ package com.app.tts.server.handler.User;
 import io.vertx.core.Handler;
 import io.vertx.rxjava.ext.web.RoutingContext;
 
-public class getBaseHandler implements Handler<RoutingContext> {
+public class GetBaseHandler implements Handler<RoutingContext> {
 
     @Override
-    public void handle(RoutingContext rc) {
-
+    public void handle(RoutingContext event) {
+        
     }
 }
