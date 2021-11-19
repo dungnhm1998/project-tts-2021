12744692ae9d8795e8a331880cef0cc749c80db0
NudePeace <lastglint@gmail.com>
2021-11-19 17:09:33 +0700

    1

--- src/main/java/com/app/tts/server/handler/User/ListBaseHandler.java
+++ src/main/java/com/app/tts/server/handler/User/ListBaseHandler.java
@@ -1,7 +1,14 @@
 package com.app.tts.server.handler.User;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.app.tts.data.type.RedisKeyEnum;
+import com.app.tts.services.ListService;
+import com.app.tts.services.OrderService;
+import com.app.tts.services.RedisService;
 import com.app.tts.session.redis.SessionStore;
 import com.app.tts.util.AppParams;
 import com.app.tts.util.ParamUtil;
@@ -16,13 +23,14 @@ public class ListBaseHandler implements Handler<RoutingContext>, SessionStore {
 
 		routingContext.vertx().executeBlocking(future ->{
 			 try{
-				 Map jsonRequest = routingContext.getBodyAsJson().getMap();
-				 
-				 String base_group_id = ParamUtil.getString(jsonRequest, AppParams.BASE_GROUP_ID);
-				 String base_name = ParamUtil.getString(jsonRequest, AppParams.BASE_NAME);
-				 
-				 List<Map> sizes = ParamUtil.getListData(jsonRequest, AppParams.SIZES);
-
+				 Map listBaseRedis = RedisService.getMap(RedisKeyEnum.BASES_MAP);
+	                if (listBaseRedis == null || listBaseRedis.isEmpty()) {
+	                    listBaseRedis = getListBaseFromDB();
+	                    routingContext.put(AppParams.RESPONSE_CODE, HttpResponseStatus.OK.code());
+	                    routingContext.put(AppParams.RESPONSE_MSG, HttpResponseStatus.OK.reasonPhrase());
+	                    routingContext.put(AppParams.RESPONSE_DATA, listBaseRedis);
+	                    future.complete();
+	                }
 	                future.complete();
 	            }catch (Exception e){
 	                routingContext.fail(e);
@@ -35,4 +43,10 @@ public class ListBaseHandler implements Handler<RoutingContext>, SessionStore {
 	            }
 	        });
 		}
+	public static Map getListBaseFromDB() throws SQLException {
+        Map listBaseDB = new HashMap();	
+        List<Map> listBaseAndGroup = ListService.getBaseSize();
+        List<Map> listBaseColor = ListService.getBaseColor();
+        List<Map> listBaseSize = ListService.getBaseGroup();
+	}
 }
