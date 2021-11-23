6611f8fe441c20993a926440d7422439c9feca18
nguyễn quý <94169574+chubekho@users.noreply.github.com>
2021-11-23 10:47:14 +0700

    campain

---
+++ src/main/java/com/app/tts/server/handler/campaign/CreateProductHandler.java
@@ -0,0 +1,43 @@
+package com.app.tts.server.handler.campaign;
+
+import com.app.tts.util.AppParams;
+import com.app.tts.util.ParamUtil;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Handler;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class CreateProductHandler implements Handler<RoutingContext> {
+
+    @Override
+    public void handle(RoutingContext rc) {
+        rc.vertx().executeBlocking(future -> {
+            try {
+                Map jsonrequest =  rc.getBodyAsJson().getMap();
+                String campaign_id = ParamUtil.getString(jsonrequest, AppParams.S_ID);
+
+
+                Map data = new HashMap();
+
+
+                rc.put(AppParams.RESPONSE_CODE, HttpResponseStatus.OK.code());
+                rc.put(AppParams.RESPONSE_MSG, HttpResponseStatus.OK.reasonPhrase());
+                rc.put(AppParams.RESPONSE_DATA, data);
+                future.complete();
+            } catch (Exception e) {
+                rc.fail(e);
+            }
+        }, asyncResult -> {
+            if (asyncResult.succeeded()) {
+                rc.next();
+            } else {
+                rc.fail(asyncResult.cause());
+            }
+        });
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(CreateProductHandler.class.getName());
+}
