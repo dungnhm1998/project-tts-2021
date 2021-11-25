4a91beb9a2f67c24cd097bba8411842fd50863a4
NudePeace <lastglint@gmail.com>
2021-11-25 10:08:35 +0700

    1

--- src/main/java/com/app/tts/server/handler/Ucant/AddProductHandler.java
+++ src/main/java/com/app/tts/server/handler/Ucant/AddProductHandler.java
@@ -18,9 +18,20 @@ public class AddProductHandler implements Handler<RoutingContext>{
             	Map json = rc.getBodyAsJson().getMap();
             	String campaign_id = ParamUtil.getString(json, AppParams.CAMPAIGN_ID);
             	String user_id = ParamUtil.getString(json, AppParams.USER_ID);
+            	String base_id = "", id = "", name = "", value = "";
+            	
             	List <Map> products = ParamUtil.getListData(json, "products");
-            	
-            	
+            	for(Map map1 : products) {
+            		base_id = ParamUtil.getString(map1, AppParams.BASE_ID);
+            		int de = ParamUtil.getBoolean(map1, AppParams.DEFAULT)? 1 : 0;
+            		List<Map> colors = ParamUtil.getListData(map1, "colors");
+            			for(Map map2 : colors) {
+            				id = ParamUtil.getString(map2, AppParams.ID);
+            				name = ParamUtil.getString(map2, AppParams.NAME);
+            				value = ParamUtil.getString(map2, AppParams.VALUE);
+            			}
+            		
+            	}
             } catch (Exception e) {
 		        rc.fail(e);
 		    }
