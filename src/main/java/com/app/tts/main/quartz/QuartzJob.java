880bac13cd56aa2f903a416fc96da167a1e249ec
NguyenPhuong11299 <93914914+NguyenPhuong11299@users.noreply.github.com>
2021-12-16 08:38:14 +0700

    ssd

--- src/main/java/com/app/tts/main/quartz/QuartzJob.java
+++ src/main/java/com/app/tts/main/quartz/QuartzJob.java
@@ -1,41 +1,47 @@
 package com.app.tts.main.quartz;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class QuartzJob /*implements Job*/ extends QuartzJobBean {
+public class QuartzJob implements Job {// extends QuartzJobBean {
 //c1
-//    @Override
-//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-//        System.out.println("--------------QuartzJob - ReadFile---------------");
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("--------------QuartzJob - ReadFile---------------");
 //        System.out.println(readOneLine());
 //        System.out.println("-------------------------------------");
-//
+
 //    }
 
-    @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("--------------QuartzJob - c1 - ReadFile---------------");
-        //c3
-        try {
-            OrderModel orderModel = readOneRecord();
-            if(orderModel != null) {
-                System.out.println("order = " + orderModel.getOrder() + " ; "
-                        + "ref = " + orderModel.getRef());
-            }else{
-                System.out.println("------------------------order = NULL----------------------------------");
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//    @Override
+//    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        System.out.println("--------------QuartzJob - c1 - ReadFile---------------");
+//        //c3
+//        try {
+//            OrderModel orderModel = readOneRecord();
+//            if(orderModel != null) {
+//                System.out.println("order = " + orderModel.getOrder() + " ; "
+//                        + "ref = " + orderModel.getRef());
+//            }else{
+//                System.out.println("------------------------order = NULL----------------------------------");
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
 // c2
+        // in ra : 2.1
 //        CSVRecord colum = ReadFile.listData.get(0);
 //        String order = colum.get(0);
 //        String ref = colum.get(1);
@@ -68,23 +74,62 @@ public class QuartzJob /*implements Job*/ extends QuartzJobBean {
 //            String trackingVal = line.get(11);
 //            String countryVal = line.get(12);
 //            String zipcodeVal = line.get(13);
-//
+
 //            System.out.println(order + " = " + orderVal + "; " +
-//                    ref + " = " + refVal + "; ");
+//                            ref + " = " + refVal + "; " +
+//                            createDate + " = " + createDateVal + "; " +
+//                            paymentDate + " = " + paymentDateVal + "; " +
+//                            productName + " = " + productNameVal + "; " +
+//                            customers + " = " + customersVal + "; " +
+//                            quantity + " = " + quantityVal + "; " +
+//                            amount + " = " + amountVal + "; " +
+//                            shippingMethod + " = " + shippingMethodVal + "; " +
+//                            state + " = " + stateVal + "; " +
+//                            fullFillState + " = " + fullFillStateVal + "; " +
+//                            tracking + " = " + trackingVal + "; " +
+//                            country + " = " + countryVal + "; " +
+//                            zipcode + " = " + zipcodeVal + "; "
+//                    );
 //        }
+
+
+        //  in ra : 2.2
+        CSVRecord line1 = readOneLine();
+
+        if(line1 != null) {
+            Map<String , String> line = line1.toMap();
+                System.out.println("order" + " = " + line.get("ORDER") + "; " +
+                        "ref" + " = " + line.get("REF") + "; " +
+                        "createDate" + " = " + line.get("CREATE DATE") + "; " +
+                        "paymentDate" + " = " + line.get("PAYMENT DATE") + "; " +
+                        "productName" + " = " + line.get("PRODUCT NAME") + "; " +
+                        "customers" + " = " + line.get("CUSTOMERS") + "; " +
+                        "quantity" + " = " + line.get("QUANTITY") + "; " +
+                        "amount" + " = " + line.get("AMOUNT") + "; " +
+                        "shippingMethod" + " = " + line.get("SHIPPING METHOD") + "; " +
+                        "state" + " = " + line.get("STATE") + "; " +
+                        "fullFillState" + " = " + line.get("FULFILL STATE") + "; " +
+                        "tracking" + " = " + line.get("TRACKING") + "; " +
+                        "country" + " = " + line.get("COUNTRY") + "; " +
+                        "zipcode" + " = " + line.get("ZIPCODE") + "; "
+                );
+
+        }
+
         System.out.println("----------------------------------------------------------");
+
     }
 //c2
-//    public static CSVRecord readOneLine()  {
-//        CSVRecord line = null;
-//        int countQuartz = ReadFile.count;
-//        List<CSVRecord> listQuartz = ReadFile.listData;
-//        if(countQuartz < listQuartz.size()){
-//            line = listQuartz.get(countQuartz);
-//            ReadFile.count += 1;
-//        }
-//        return line;
-//    }
+    public static CSVRecord readOneLine()  {
+        CSVRecord line = null;
+        int countQuartz = ReadFile.count;
+        List<CSVRecord> listQuartz = ReadFile.listData;
+        if(countQuartz < listQuartz.size()){
+            line = listQuartz.get(countQuartz);
+            ReadFile.count += 1;
+        }
+        return line;
+    }
 
     public static OrderModel readOneRecord() throws FileNotFoundException {
         OrderModel orderModel = null;
--- src/main/java/com/app/tts/main/quartz/ReadFile.java
+++ src/main/java/com/app/tts/main/quartz/ReadFile.java
@@ -15,16 +15,40 @@ public class ReadFile {
     public static int count2 = 1;
     public static void readFile() {
         String csvFile = "E:\\springboot\\createOrderData2.csv"; //Orders_export_1639471684871.csv";
-        try(Reader reader = new FileReader(csvFile);
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-        ) {
-           Iterable<CSVRecord> csvRecords = csvParser.getRecords();
-           for(CSVRecord csvRecord : csvRecords){
-               listData.add(csvRecord);
+        try{
+            Reader reader = new FileReader(csvFile);
+            CSVParser csvParser = null;
+                try{
+                     csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());//.withHeader() chi dinh key = ten cot
+                    Iterable<CSVRecord> csvRecords = csvParser.getRecords();
 
-               System.out.println("------------readFile-------------");
-               System.out.println("-------------------------------");
-           }
+                    for(CSVRecord csvRecord : csvRecords){
+                        listData.add(csvRecord);
+
+
+                        System.out.println("------------test-loop-------------");
+
+                        System.out.println(csvRecord.get(0));
+                        System.out.println("-------------------------------");
+                    }
+            }catch (IllegalArgumentException e){
+                    // chi chap nhan 1 cot k co nameColumn con lai se nhay vao ngoai le nay
+                    System.out.println("---------------end-----------");
+                    System.out.println("khong co header");
+                    System.out.println("------------------------------------------");
+                    System.out.println("----------------------------------------------");
+                }
+//           Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+//
+//           for(CSVRecord csvRecord : csvRecords){
+//               listData.add(csvRecord);
+//
+//
+//               System.out.println("------------readFile-------------");
+//
+//               System.out.println(csvRecord.get(0));
+//               System.out.println("-------------------------------");
+//           }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
